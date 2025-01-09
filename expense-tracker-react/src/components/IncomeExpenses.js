0bdb9166ddb8c56885a6c6da1098596e06842b53
Deepak <deepakrajesh567@gmail.com>
2025-01-09 11:53:06 +0900

    UPDATED: Income and expense depending on the total transaction amounts

--- expense-tracker-react/src/components/IncomeExpenses.js
+++ expense-tracker-react/src/components/IncomeExpenses.js
@@ -1,13 +1,25 @@
+import { GlobalContext } from "../Context/GlobalState"
+import { useContext } from "react"
+
 export default function IncomeExpenses(){
-    return(
+  const { transactions } = useContext(GlobalContext)
+  let income = 0
+  let expense = 0
+  transactions.forEach(transaction => {
+      if(transaction.amount >0)
+        income+=transaction.amount
+      else
+        expense+=Math.abs(transaction.amount)
+  });
+  return(
         <div className="inc-exp-container">
         <div>
           <h4>Income</h4>
-          <p className="money plus">+$0.00</p>
+          <p className="money plus">+${income}</p>
         </div>
         <div>
           <h4>Expense</h4>
-          <p className ="money minus">-$0.00</p>
+          <p className ="money minus">-${expense}</p>
         </div>
       </div>
     )
