e02b12fe7b720db8d8348a5bb4b5e3bd9276b72a
Deepak <deepakrajesh567@gmail.com>
2025-01-09 11:44:22 +0900

    ADDED:Balance by iterating through the array

--- expense-tracker-react/src/components/Balance.js
+++ expense-tracker-react/src/components/Balance.js
@@ -1,8 +1,24 @@
+import { useContext } from "react"
+import { GlobalContext } from "../Context/GlobalState"
+
+
+
 export default function Balance(){
+    const { transactions } = useContext(GlobalContext)
+    let income = 0
+    let expense = 0
+    transactions.forEach(transaction =>{
+        if(transaction.amount>0)
+            income+=transaction.amount
+        else
+        expense+=Math.abs(transaction.amount)
+    })
+    console.log(income)
+    console.log(expense)
     return(
         <>
             <h4>Your Balance</h4>
-            <h1>$0.00</h1>
+            <h1>${income-expense}</h1>
         </>
     )
 }
