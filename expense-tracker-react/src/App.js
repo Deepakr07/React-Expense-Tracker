5c9ed9af237011d85d32b8ac35d204d31c89e031
Deepak <deepakrajesh567@gmail.com>
2025-01-09 10:54:28 +0900

    ADD: transaction data

--- expense-tracker-react/src/App.js
+++ expense-tracker-react/src/App.js
@@ -1,13 +1,15 @@
-import Header from "./components/Header";
-import Balance from "./components/Balance";
-import IncomeExpenses from "./components/IncomeExpenses";
-import TransactionList from "./components/TransactionList";
-import AddTransaction from "./components/AddTransaction";
+import Header from "./Components/Header";
+import Balance from "./Components/Balance";
+import IncomeExpenses from "./Components/IncomeExpenses";
+import TransactionList from "./Components/TransactionList";
+import AddTransaction from "./Components/AddTransaction";
+import { GlobalProvider } from "./Context/GlobalState";
+
 import "./App.css"
 
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header />
       <div className="container">
         <Balance />
@@ -15,7 +17,7 @@ function App() {
         <TransactionList />
         <AddTransaction />
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
--- expense-tracker-react/src/components/TransactionList.js
+++ expense-tracker-react/src/components/TransactionList.js
@@ -1,11 +1,18 @@
+import { useContext } from "react"
+import { GlobalContext } from "../Context/GlobalState"
 export default function TransactionList(){
+    const { transactions } = useContext(GlobalContext)
+
+    const transactionData = transactions.map((transaction,key)=>{
+        return <li className="minus" key={key}>
+        {transaction.text} <span>-$400</span><button className="delete-btn">x</button>
+            </li> 
+    })
     return(
         <>
         <h3>History</h3>
             <ul className="list">
-                <li className="minus">
-            Cash <span>-$400</span><button className="delete-btn">x</button>
-                </li> 
+                {transactionData}
             </ul>
         </>
     )
