be94429787fb93fc97b3788ef056516c43176d2a
Deepak <deepakrajesh567@gmail.com>
2025-01-09 16:36:17 +0900

    UPDATE : Normal form to useform

--- expense-tracker-react/src/components/AddTransaction.js
+++ expense-tracker-react/src/components/AddTransaction.js
@@ -1,35 +1,41 @@
 import { GlobalContext } from "../Context/GlobalState"
 import { useState,useContext } from "react"
+import { useForm } from 'react-hook-form'
 export default function AddTransaction(){
-    const [text, setText] = useState('')
-    const [amount, setAmout] = useState(0)
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        reset
+    } = useForm();
+    
     const { addTransaction } = useContext(GlobalContext)
 
-    const onSubmit = e =>{
-        e.preventDefault()
-        const newTransaction = {
-            id:Math.floor(Math.random() * 1000000000),
+    const onSubmit = ({text,amount}) => {
+        const newTransaction ={
+            id:Math.floor(Math.random()*100000000000),
             text,
             amount
         }
-        setText('')
-        setAmout(0)
         addTransaction(newTransaction)
-    }
+        reset()
+   
+      };
+
 
     return(
         <>
             <h3>Add new transaction</h3>
-            <form onSubmit={onSubmit}>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="form-control">
                     <label htmlFor = "text">Text</label>
-                    <input type = "text" value = {text} onChange = {(e) => setText(e.target.value)} placeholder = "Enter text..."/>
+                    <input type = "text" name = "text" {...register("text")}placeholder = "Enter text..."/>
                 </div>
                 <div className="form-control">
                     <label htmlFor = "amount">Amount <br />
                         (negative - expense, positive - income)
                     </label>
-                    <input type="number" value = {amount} onChange = {(e) => setAmout(e.target.value)}placeholder="Enter amount..." />
+                    <input type="number" name = "amount"  {...register("amount")}placeholder="Enter amount..." maxLength={10}/>
                 </div>
                 <button className="btn">Add transaction</button>
             </form>            
