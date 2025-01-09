8da1047999add40bc5ae4991e7208402f5f771b3
Deepak <deepakrajesh567@gmail.com>
2025-01-09 11:17:40 +0900

    DISPLAYED : right side border color of each transaction depending on the nature of each transaction

--- expense-tracker-react/src/components/Transaction.js
+++ expense-tracker-react/src/components/Transaction.js
@@ -1,7 +1,9 @@
 export default function Transaction({ transaction}){
+    const sign = transaction.amount>0 ? '+':'-'
+    const className = transaction.amount > 0 ? "minus" : "plus"
     return(
-        <li className="minus">
-        {transaction.text} <span>-$400</span><button className="delete-btn">x</button>
+        <li className = {className}>
+        {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span><button className="delete-btn">x</button>
             </li> 
     )
 }
