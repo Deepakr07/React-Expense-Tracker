624cf450d7b752047df855a106a8d15474e45de6
Deepak <deepakrajesh567@gmail.com>
2025-02-18 11:13:57 +0900

    completed styling the income expense page

--- expense-tracker-react/src/Pages/IncomeExpensePage/Components/Transaction.jsx
+++ expense-tracker-react/src/Pages/IncomeExpensePage/Components/Transaction.jsx
@@ -2,40 +2,60 @@ import { useContext } from "react";
 import { GlobalContext } from "../../../Context/GlobalState";
 import { ListItem } from "@mui/material";
 import Tooltip from "./Tooltip";
-import "./Transaction.css"
-import { ArrowCircleDown } from "../../../Icons/icons";
+import "./Transaction.css";
+import { ArrowCircleDown, ArrowCircleUp } from "../../../Icons/icons";
 
 export default function Transaction({ transaction }) {
+
   const { deleteTransaction } = useContext(GlobalContext);
   const sign = transaction.amount > 0 ? "+" : "-";
-  // const className = transaction.amount > 0 ? "plus" : "minus";
+  const className = transaction.amount > 0 ? "income" : "expense";
+  const circleClass = transaction.amount > 0 ? "green-background" : "red-background";
+  const Icon = transaction.amount > 0 ? ArrowCircleUp : ArrowCircleDown;
 
   function truncateTransactionEntity(text) {
     return text.length > 17 ? `${text.slice(0, 17)}...` : text;
   }
+
+
   return (
     <div className="transaction-card">
       <Tooltip amount={transaction.text}>
         {" "}
-          <ListItem sx={{borderRadius:"10px",border:"2px solid red",padding:"2rem",margin:"2px"}}>
-        <div className="transaction-container">
-          <div className="left-transaction">
-            <div className="circle-container">
-              <ArrowCircleDown />
+        <ListItem
+          sx={{
+            borderRadius: "10px",
+            // border: "2px solid red",
+            padding: "1.7rem",
+            margin: "2px",
+            fontWeight: "600",
+          }}
+          className={className}
+        >
+          <div className={`transaction-container`}>
+            <div className="left-transaction">
+              <div className={circleClass}>
+                <Icon
+                  sx={{
+                    fontSize: "1.2em",
+                  }}
+                />
+              </div>
+              <div className="text-container">
+                <span>{truncateTransactionEntity(transaction.text)}</span>
+                <span className="date">2024-10-29</span>
+              </div>
             </div>
-            {truncateTransactionEntity(transaction.text)}
+            <div className={className}>
+              {sign}${Math.abs(transaction.amount)}
+            </div>
           </div>
-              <span>
-                {sign}${Math.abs(transaction.amount)}
-              </span>
-        </div>
-            <button
-              className="delete-btn"
-              onClick={() => deleteTransaction(transaction.id)}
-            >x
-            </button>
-          </ListItem>
-        {" "}
+          <button
+            className="delete-btn"
+            onClick={() => deleteTransaction(transaction.id)}
+          >x
+          </button>
+        </ListItem>{" "}
       </Tooltip>
     </div>
   );
