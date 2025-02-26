d40f0308af083ce902cf7f0d33cbcf982edad69f
Deepak <deepakrajesh567@gmail.com>
2025-02-26 15:05:19 +0900

    removed: redundant components and css files from the respective components folder as they are globally available

--- src/Core/Helpers/validations.js
+++ src/Core/Helpers/validations.js
@@ -28,7 +28,10 @@ export const handleAmountBeforeInput = (e) => {
     }
   };
 
-  export const getCurrentDateAndTime = ()=>{
-    const formattedDateTime = new Date().toLocaleDateString("en-GB").replace(/\//g, "-") + " " + new Date().toLocaleTimeString("en-GB", { hour: "2-digit", minute: "2-digit", hour12: false });
+  export const getCurrentDateAndTime = () =>{
+    const formattedDateTime = new Date()
+          .toLocaleDateString("en-GB")
+          .replace(/\//g, "-") + " " + new Date()
+          .toLocaleTimeString("en-GB", { hour: "2-digit", minute: "2-digit", hour12: false });
     return formattedDateTime
   }
