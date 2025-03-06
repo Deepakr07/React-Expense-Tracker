4e39ff1878aae9391276f4c4f3df362fd08b33ea
Deepak <deepakrajesh567@gmail.com>
2025-03-06 09:00:36 +0900

    Posted transaction and displayed a snackbar on successful post | formatted code - made it more readable

--- src/Pages/History/HistoryPage.jsx
+++ src/Pages/History/HistoryPage.jsx
@@ -2,11 +2,21 @@ import TransactionList from "../../Components/TransactionList";
 import { useQuery } from "@tanstack/react-query";
 import { getExpenses } from "@/Actions/expenseActions";
 
-export default function HistoryPage(){
-    const { data } = useQuery({queryKey:['Transaction History'],queryFn: ()=>getExpenses(1,10,null)})
-    if(data) console.log("From history page",data)
+export default function HistoryPage() {
 
-    return(<div className="container">
-        <TransactionList incomeExpenseTransaction = {false} title = "Transaction History" buttonOnClick = "{editTransaction}" transactions={data?.data}/>
-    </div>)
+  const { data } = useQuery({
+    queryKey: ["Transaction History"],
+    queryFn: () => getExpenses(1, 10, null),
+  });
+
+  return (
+    <div className="container">
+      <TransactionList
+        incomeExpenseTransaction={false}
+        title="Transaction History"
+        buttonOnClick="{editTransaction}"
+        transactions={data?.data}
+      />
+    </div>
+  );
 }
